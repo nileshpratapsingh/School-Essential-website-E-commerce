a173f070a062ea9e8d994c245a6ec312ecd1d74b
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-03-10 03:42:56 +0530

    Updated

--- script.js
+++ script.js
@@ -9,9 +9,17 @@
     document.getElementById("signup-form").style.display = "block"; // Show signup form
   }
 
-
   function toggleMenu() {                                         //Function to toggle sidebar
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('active');
   }
-    
+
+  function showBoyBag() {
+    document.getElementById('bagImage').src = 'bott';
+    document.getElementById('bagImage').alt = 'Boy Bag';
+  }
+
+  function showGirlBag() {
+    document.getElementById('bagImage').src = 'girl-bag.jpg';
+    document.getElementById('bagImage').alt = 'Girl Bag';
+  }
