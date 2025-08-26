50cd7aa8d5ac93c91a008f3cffc0093a4be1a2bd
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-08-27 01:19:52 +0530

    error handling ans maintainance

--- backend/controller/admin.controller.mjs
+++ backend/controller/admin.controller.mjs
@@ -1,18 +1,19 @@
-import { Login } from "../models/user.model.mjs";
+import { Login, signup } from "../models/user.model.mjs";
 
 function dashboardRoute(req, res) {
-  res.render("admin/admin-dashboard", { pageTitle: null });
+  res.render("admin/admin-dashboard", { pageTitle: "Admin-Dashboard" });
 }
 
-async function dashboardtrigger() {
+async function dashboardtoggle() {
   try {
     const admin = await Login.findOne({
       name: "Admin",
       passWord: "Kvw>_Eob&8<dXoYS<vB4",
+      role: "admin",
     });
 
     if (admin) {
-      res.redirect("/dasboard");
+      res.redirect("/admin-dasboard");
       return true;
     } else {
       console.log("Model Error!!!");
@@ -26,9 +27,32 @@ async function dashboardtrigger() {
   }
 }
 
+async function usersList(req, res) {
+  try {
+    let users = await signup.find();
+    if (!users) {
+      res.status(404).send({
+        statusCode: 404,
+        statusText: "Users not Found!!",
+        message: "Corrupted data or Users list must be empty.",
+        errorDetails: "Check Connections...",
+      });
+    }
+    res.render("admin/users-list", { users });
+  } catch (error) {
+    console.log("Error in Users List", error.message);
+    return res.status(500).send({
+      statusCode: 500,
+      statusText: "Check admin Controller",
+      errorDetails: null,
+    });
+  }
+}
+
 const adminController = {
+  usersList,
   dashboardRoute,
-  dashboardtrigger,
+  dashboardtoggle,
 };
 
 export default adminController;
