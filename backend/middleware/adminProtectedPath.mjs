e0c25ae93e5c738f013e272a18abc863a0e5f420
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-08-27 01:15:07 +0530

    maintainance and debugging

--- backend/middleware/adminProtectedPath.mjs
+++ backend/middleware/adminProtectedPath.mjs
@@ -4,31 +4,47 @@ import { config } from "../config/config.mjs";
 
 export async function adminProtectedPath(req, res, next) {
   try {
-    let token = req.cookies?.token || req.headers["authorization"];
+       let token =
+      req.cookies?.refreshToken ||
+      req.headers["authorization"];
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized, Requires Admin !!!" });
+      return next({
+        status: 401,
+        statusText: "Unauthorized",
+        message: "You must be logged in as admin to access this page.",
+        errorDetails: "Token missing or invalid.",
+      });
     }
 
     if (token.startsWith("Bearer ")) {
       token = token.split(" ")[1];
     }
 
-    const decoded = jwt.verify(token, config.jwt.secret);
-    const user = await signup.findOne({ email: decoded.email });
+    const decoded = jwt.verify(token, config.jwt.refreshSecret);
+    const user = await signup.findById(decoded.userId);
 
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
+      return next({
+        status: 401,
+        statusText: "Unauthorized",
+        message: "You must be logged in as admin to access this page.",
+        errorDetails: "User not found or invalid user details.",
+      });
     }
 
     if (user.role !== "admin") {
-      return res.status(403).json({ message: "Forbidden — Admins only" });
+      return next({
+        status: 403,
+        statusText: "Unauthorized",
+        message: "Forbidden - Admin only area",
+        errorDetails: "Admin not found or invalid user details.",
+      });
     }
 
     req.user = user;
     next();
-
   } catch (err) {
-    res.status(401).json({ error: err.message });
+    res.status(401).json({ errorDetails: err.message });
   }
 }
