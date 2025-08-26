2dea77b751965ded0e4f9656d12fa505009fe215
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-08-27 01:13:12 +0530

    created middelware for 404 not found errors

---
+++ backend/middleware/404notFoundhandler.mjs
@@ -0,0 +1,8 @@
+export function notFoundHandler(req, res, next) {
+  res.status(404).render("pages/error", {
+    statusCode: 404,
+    statusText: "Not Found",
+    message: "The page you are looking for does not exist.",
+    errorDetails: "May be underconstruction...",
+  });
+}
