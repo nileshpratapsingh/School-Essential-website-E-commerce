b68d393d0c2e740cadaff72a6dbe8fd959a11f3e
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-08-27 01:04:44 +0530

    added new routes with middlewares

--- backend/routes/admin.mjs
+++ backend/routes/admin.mjs
@@ -1,8 +1,9 @@
 import express from "express";
 import adminController from "../controller/admin.controller.mjs";
 import { adminProtectedPath } from "../middleware/adminProtectedPath.mjs";
+
 const adminRouter = express.Router();
 
-adminRouter.route("/admin").get(adminProtectedPath,adminController.dashboardtrigger);
-
+adminRouter.route("/admin").get(adminProtectedPath,adminController.dashboardtoggle);
+adminRouter.route("/users-list").get(adminProtectedPath,adminController.usersList);
 export default adminRouter;
