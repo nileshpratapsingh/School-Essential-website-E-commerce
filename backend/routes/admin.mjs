daf74b19016efeadc4df646a91290a537a9b2833
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-10-26 22:44:17 +0530

    Added the Admin triggers and some admin feature to website

--- backend/routes/admin.mjs
+++ backend/routes/admin.mjs
@@ -1,9 +1,22 @@
 import express from "express";
+import cloudinaryUpload from "../middleware/cloudinaryUpload.mjs";
 import adminController from "../controller/admin.controller.mjs";
 import { adminProtectedPath } from "../middleware/adminProtectedPath.mjs";
 
 const adminRouter = express.Router();
 
-adminRouter.route("/admin").get(adminProtectedPath,adminController.dashboardtoggle);
-adminRouter.route("/users-list").get(adminProtectedPath,adminController.usersList);
+adminRouter
+  .route("/admin")
+  .get(adminController.dashboardtoggle);
+adminRouter
+  .route("/users-list")
+  .get(adminProtectedPath, adminController.usersList);
+adminRouter
+  .route("/add-product")
+  .get(adminProtectedPath, adminController.addProductRoute)
+  .post(
+    adminProtectedPath,
+    cloudinaryUpload.single("productImage"),
+    adminController.addProduct
+  );
 export default adminRouter;
