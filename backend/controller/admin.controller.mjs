79859f7ed859d0a203940cc5916ee318a0fd9de2
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-10-26 23:01:06 +0530

    Added the feature to check live users,total users,adding product and error handling

--- backend/controller/admin.controller.mjs
+++ backend/controller/admin.controller.mjs
@@ -1,31 +1,51 @@
+//Import Modules
+
+import jwt from "jsonwebtoken";
+
+// Import Configuration
+
+import { config } from "../config/config.mjs";
+
+// Import Models
+
+import Product from "../models/product.model.mjs";
 import { Login, signup } from "../models/user.model.mjs";
+
+// Admin Dashboard Route
 
 function dashboardRoute(req, res) {
   res.render("admin/admin-dashboard", { pageTitle: "Admin-Dashboard" });
 }
 
-async function dashboardtoggle() {
-  try {
-    const admin = await Login.findOne({
-      name: "Admin",
-      passWord: "Kvw>_Eob&8<dXoYS<vB4",
-      role: "admin",
-    });
+// Admin Features response
 
-    if (admin) {
-      res.redirect("/admin-dasboard");
-      return true;
-    } else {
-      console.log("Model Error!!!");
-      res.status(401).send("Unauthorized");
-      return false;
+async function dashboardtoggle(req, res) {
+  try {
+    const token =
+      req.cookies.refreshToken || req.headers.authorization?.split(" ")[1];
+
+    if (!token) return res.status(401).send(false);
+
+    let decoded;
+    try {
+      decoded = jwt.verify(token, config.jwt.refreshSecret); // try as refresh
+    } catch (err) {
+      decoded = jwt.verify(token, config.jwt.secret); // try as access
+    }
+    const userRole = await Login.findOne(decoded.email);
+    if (userRole.role === "admin") {
+      console.log(userRole.role);
+      return res.send(true);
     }
   } catch (err) {
+    console.log("Check admin controller dashboard trigger");
     console.error("Database Error:", err);
     res.status(500).send("Server Error");
     return false;
   }
 }
+
+// Users List Route
 
 async function usersList(req, res) {
   try {
@@ -38,9 +58,20 @@ async function usersList(req, res) {
         errorDetails: "Check Connections...",
       });
     }
-    res.render("admin/users-list", { users });
+
+    const userStatus = await Login.find({}, "email -_id");
+
+    const totalUsers = await signup.countDocuments();
+
+    res.render("admin/users-list", {
+      pageTitle: "Users List",
+      users,
+      totalUsers,
+      userStatus,
+    });
   } catch (error) {
     console.log("Error in Users List", error.message);
+
     return res.status(500).send({
       statusCode: 500,
       statusText: "Check admin Controller",
@@ -49,7 +80,70 @@ async function usersList(req, res) {
   }
 }
 
+// Add Product Route
+
+function addProductRoute(req, res) {
+  res.render("admin/add-product", { pageTitle: "Add Products" });
+}
+
+// Add Product Logic
+
+async function addProduct(req, res, next) {
+  try {
+    const {
+      title,
+      category,
+      subCategory,
+      rating,
+      price,
+      description,
+      features,
+      specKeys,
+      specValues,
+    } = req.body;
+
+    const totalProducts = await Product.countDocuments();
+    
+    const id = totalProducts === 0 ? 1 : totalProducts + 1;
+
+    const specs = {};
+    if (Array.isArray(specKeys)) {
+      specKeys.forEach((key, i) => {
+        if (key.trim() !== "") specs[key] = specValues[i];
+      });
+    }
+
+    const newProduct = new Product({
+      id,
+      title,
+      category,
+      subCategory,
+      rating,
+      price,
+      productImage: req.file?.path || null, // Cloudinary URL,
+      description,
+      features,
+      specs,
+    });
+    console.log(req.file?.path);
+
+    await newProduct.save();
+    res.redirect("/product");
+  } catch (error) {
+    console.log("Check admincontroller addproduct\n", error.message);
+    return res.status(500).send(
+      next({
+        statusCode: 500,
+        statusText: "Check admin Controller addProduct",
+        errorDetails: error.message,
+      })
+    );
+  }
+}
+
 const adminController = {
+  addProduct,
+  addProductRoute,
   usersList,
   dashboardRoute,
   dashboardtoggle,
