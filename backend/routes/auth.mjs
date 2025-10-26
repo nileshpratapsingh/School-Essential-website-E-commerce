3c6531d2a2ff9f65520718cf36a54698c4e594e0
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-10-26 22:45:49 +0530

    Added the cloudinary middleware for image upload and new trigger features

--- backend/routes/auth.mjs
+++ backend/routes/auth.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import authController from "../controller/auth.controller.mjs";
 import adminController from "../controller/admin.controller.mjs";
+import cloudinaryUpload from "../middleware/cloudinaryUpload.mjs";
 import { adminProtectedPath } from "../middleware/adminProtectedPath.mjs";
 import { restrictedAfterLogin } from "../middleware/loginRestriction.mjs";
 import { loginProtectedPath } from "../middleware/loginProtectedPath.mjs";
@@ -9,24 +10,34 @@ const authRouter = express.Router();
 
 authRouter
   .route("/signup")
-  .get(restrictedAfterLogin,authController.SignUpRoute)
-  .post(authController.SignUpProcedure);
+  .get(restrictedAfterLogin, authController.SignUpRoute)
+  .post(
+    cloudinaryUpload.single("profileImage"),
+    authController.SignUpProcedure
+  );
 
 authRouter
   .route("/login")
-  .get(restrictedAfterLogin,authController.loginRoute)
+  .get(restrictedAfterLogin, authController.loginRoute)
   .post(authController.loginProcedure);
 
 authRouter
   .route("/logout")
-  .get(loginProtectedPath,authController.logoutRoute);
+  .get(loginProtectedPath, authController.logoutRoute);
 
 authRouter
   .route("/profile")
-  .get(loginProtectedPath,authController.profileRoute);
+  .get(loginProtectedPath, authController.profileRoute);
 
 authRouter
+  .route("/edit-profile/:id")
+  .get(loginProtectedPath, authController.editProfile);
+authRouter
   .route("/admin-dashboard")
-  .get(adminProtectedPath,adminController.dashboardRoute);
+  .get(adminProtectedPath, adminController.dashboardRoute);
+
+authRouter
+  .route("/auth")
+  .get(authController.authButtonToggle);
 
 export default authRouter;
