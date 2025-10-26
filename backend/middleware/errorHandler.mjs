599e865cb353cc6bbe3e316e76bb48f46f484a6a
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-10-26 22:56:43 +0530

    Added the login button for feature for error page

--- backend/middleware/errorHandler.mjs
+++ backend/middleware/errorHandler.mjs
@@ -1,14 +1,15 @@
-
 export function errorHandler(err, req, res, next) {
   const statusCode = err.status || 500;
   const statusText = err.statusText || "Internal Server Error";
   const message = err.message || "Something went wrong!";
   const errorDetails = err.errorDetails || null;
+  const loginButton = err.loginButton || false;
 
   res.status(statusCode).render("pages/error", {
     statusCode,
     statusText,
     message,
     errorDetails,
+    loginButton,
   });
 }
