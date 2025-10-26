410cd8af6ac7391053104e6da423603090caeea2
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-10-27 00:41:41 +0530

    Cloudianry middelware for image upload

---
+++ backend/config/cloudinary.mjs
@@ -0,0 +1,11 @@
+// middleware/cloudinary.mjs
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "../config/config.mjs";
+
+cloudinary.config({
+  cloud_name: config.cloudinary.cloud_name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret,
+});
+
+export default cloudinary;
