d921f42b66cad93ea0b803582aeab225e311c35b
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-02-26 03:37:31 +0530

    Made a security middleware registor to make codebase better and easy to understand and debug

---
+++ backend/utility/registerSecurityMiddleware.mjs
@@ -0,0 +1,26 @@
+/*
+ *  Register security middleware
+ */
+
+// Security Modules
+
+// import xss from "xss-clean";
+// import helmet from "helmet";
+// import mongoSanitize from "express-mongo-sanitize";
+// import rateLimit from "express-rate-limit";
+//
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 1000,
+//     message: "Too many requests from this IP, please try again later.",
+// });
+//
+// export default securityMiddleWares(app){
+//     // Security middlewares
+//     app.use([
+//         xss(),
+//         helmet(),
+//         mongoSanitize(),
+//         limiter,
+//     ]);
+// }
