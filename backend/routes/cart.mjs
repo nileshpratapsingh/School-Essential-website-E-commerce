62e5de70541b2604d14239fef863efae0aa0f40e
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:25:18 +0530

    converted the traditional express router to dynamic, easily maintaianable and object oriented

--- backend/routes/cart.mjs
+++ backend/routes/cart.mjs
@@ -1,19 +1,55 @@
 import express from "express";
-import cartController from "../controller/cart.controller.mjs";
+import { CartController } from "../controller/cart.controller.mjs";
 import { loginProtectedPath } from "../middleware/loginProtectedPath.mjs";
-const cartRouter = express.Router();
 
-cartRouter
-  .route("/cart")
-  .get(loginProtectedPath, cartController.cartRoute)
-  .post(loginProtectedPath, cartController.saveCart);
+const CC = new CartController();
 
-cartRouter
-  .route("/delete-item")
-  .post(cartController.deleteItem);
-cartRouter
-  .route("alter-quatity")
-  .post(cartController.alterQuantity);
+class CartRouter{
+    /*
+     * Route and their methods as private member of class
+     * Better for maintainance and adding more routes & methods in future
+     */
 
-export default cartRouter;
+    //Open post route and methods
+    #openPostRoutesAndMethods={
+        "/delete-item":CC.deleteItem,
+        "/alter-quantity":CC.alterQuantity,
+    };
+    //Login protected get routes and methods
+    #LoginProtectedGetRoutesAndMethods={
+        "/cart":CC.cartRoute,
+    };
+    //Login protected post route and methods
+    #LoginProtectedPostRouteAndMethods= {
+        "/cart":CC.saveCart,
+    };
 
+    constructor(){
+        this.router = express.Router();
+        this.initializeRoutes();
+    }
+
+    initializeRoutes(){
+
+        // Open GET
+        Object.entries(this.#openPostRoutesAndMethods).forEach(([path, handler])=>{
+            this.router
+                .route(path)
+                .post(handler)
+        })
+        // Login Protected GET
+        Object.entries(this.#LoginProtectedGetRoutesAndMethods).forEach(([path, handler])=>{
+            this.router
+                .route(path)
+                .get(loginProtectedPath, handler)
+        })
+        //Login Protected POST
+        Object.entries(this.#LoginProtectedPostRouteAndMethods).forEach(([path, handler])=>{
+            this.router
+                .route(path)
+                .post(loginProtectedPath, handler)
+        })
+    }
+}
+
+export default new CartRouter().router;
