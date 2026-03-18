ef460d48ea5765bf04ef904042c5773ebbf16d89
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:26:41 +0530

    converted the traditional express router to dynamic, easily maintaianable and object oriented

--- backend/routes/views.mjs
+++ backend/routes/views.mjs
@@ -1,24 +1,71 @@
 import express from "express";
-import viewsControllers from "../controller/views.controller.mjs";
+import { ViewsController } from "../controller/views.controller.mjs";
 import { loginProtectedPath } from "../middleware/loginProtectedPath.mjs";
-const router = express.Router();
 
-// GET routes
-router.route("/").get(viewsControllers.indexRoute);
-router.route("/uniform").get(viewsControllers.uniformRoute);
-router.route("/stationary").get(viewsControllers.stationaryRoute);
-router.route("/feedback").get(loginProtectedPath,viewsControllers.feedbackRoute);
-router.route("/business-enquiry").get(loginProtectedPath,viewsControllers.businessEnquiryRoute);
-router.route("/mobile-app").get(viewsControllers.mobileAppRoute);
-router.route("/contact").get(loginProtectedPath,viewsControllers.contactRoute);
-router.route("/chatbot").get(loginProtectedPath,viewsControllers.chatbotRoute);
-router.route("/about").get(viewsControllers.aboutRoute);
-router.route("/account").get(loginProtectedPath,viewsControllers.accountRoute);
-router.route("/refresh-token").get(viewsControllers.refreshTokenRoute)
-router.route("/api").get(viewsControllers.configAPIUrl);
-// POST routes
-router.route("/contact/contact_form").post(viewsControllers.contactingMessage);
+const VC = new ViewsController();
 
-// PUT/DELETE routes here
+class ViewRouter {
+    /*
+     * Route and their methods as private member of class
+     * Better for maintainance and adding more routes & methods in future
+     */
 
-export default router;
+    //Open get route and methods
+    #openGetRoutesAndMethods={
+        "/":VC.indexRoute,
+        "/api":VC.configAPIUrl,
+        "/about":VC.aboutRoute,
+        "/uniform":VC.uniformRoute,
+        "/mobile-app":VC.mobileAppRoute,
+        "/stationary":VC.stationaryRoute,
+        "/refresh_token":VC.refreshTesting,
+        "/contact/contact_form":VC.contactingMessage,
+    }
+    //Open post route and methods 
+    #openPostRoutesAndMethods={
+        "/refresh_token":VC.refreshTokenRoute,
+        "/contact/contact_form":VC.contactingMessage,
+    }
+    //Login protected get routes and methods
+    #LoginProtectedGetRoutesAndMethods={
+        "/contact":VC.contactRoute,
+        "/chatbot":VC.chatbotRoute,
+        "/account":VC.accountRoute,
+        "/feedback":VC.feedbackRoute,
+        "/business-enquiry":VC.businessEnquiryRoute,
+    }
+    //login protected post routes and methods
+    #LoginProtectedPostRoutesAndMethods={
+        "/feedback":VC.feedbackRoute,
+    }
+
+    constructor(){
+        this.router = express.Router();
+        this.initializeRoutes();
+    }
+
+    initializeRoutes(){
+        Object.entries(this.#openGetRoutesAndMethods).forEach(([path, handler]) => {
+            this.router
+                .route(path)
+                .get(handler);
+        });
+        Object.entries(this.#openPostRoutesAndMethods).forEach(([path, handler]) => {
+            this.router
+                .route(path)
+                .post(handler);
+        });
+        Object.entries(this.#LoginProtectedGetRoutesAndMethods).forEach(([path, handler]) => {
+            this.router
+                .route(path)
+                .get(loginProtectedPath, handler);
+        });
+        Object.entries(this.#LoginProtectedPostRoutesAndMethods).forEach(([path, handler])=>{
+            this.router
+                .route(path) 
+                .post(loginProtectedPath, handler)
+        });
+    }
+}
+
+export default new ViewRouter().router;
