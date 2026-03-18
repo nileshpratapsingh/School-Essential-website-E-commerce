109f1f50a1f889dbb13d0e64d8d1654ffe73ca35
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:39:39 +0530

    Utility integration.

--- backend/middleware/loginProtectedPath.mjs
+++ backend/middleware/loginProtectedPath.mjs
@@ -1,56 +1,62 @@
-import jwt from "jsonwebtoken";
-import { config } from "../config/config.mjs";
+
 import { signup } from "../models/user.model.mjs";
+import { TokenUtility } from "../utility/tokenUtility.mjs";
 
 export async function loginProtectedPath(req, res, next) {
-  try {
-    let token = req.cookies?.refreshToken || req.headers["authorization"];
+    try {
+        let token = TokenUtility.getToken(req);
 
-    if (!token) {
-      return next({
-        status: 401,
-        statusText: "Unauthorized",
-        message: "You must be logged in to access this page.",
-        errorDetails: "Token missing or invalid.",
-        loginButton: true,
-      });
+        if (!token) {
+            return next({
+                status: 401,
+                statusText: "Unauthorized",
+                message: "You must be logged in to access this page.",
+                errorDetails: "Token missing or invalid.",
+                loginButton: true,
+            });
+        }
+
+        if (typeof token === "string" && token?.startsWith("Bearer ")) {
+            token = token.split(" ")[1];
+        }
+
+        const decoded = TokenUtility.verifyToken(token);
+        const user = await signup.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return next({
+                status: 401,
+                statusText: "Unauthorized",
+                message: "You must be logged in to access this page.",
+                errorDetails: "User missing or invalid user.",
+                loginButton: true,
+            });
+        }
+
+        req.user = user; // store user info for next middlewares
+        req.userRole = decoded.role;
+
+        next();
+    } catch (error) {
+        let token = TokenUtility.getToken(req);
+        const decoded = TokenUtility.verifyToken(token);
+        const user = await signup.findById(decoded.userId).select("-password");
+
+        if (error.name === "TokenExpiredError") {
+            return res
+                .status(401)
+                .json({ message: "Token expired, please log in again." });
+        }
+        if (error.name === "JsonWebTokenError") {
+            return res.status(401).json({ message: "Invalid token. from loginProtectedpath" ,
+                error:error,
+            });
+        }
+        return next({
+            status: 500,
+            statusText: `details :${token||null}`,
+            message: "Check middlewares loginprotectedpath",
+            errorDetails: error,
+        });
     }
-
-    if (typeof token === "string" && token?.startsWith("Bearer ")) {
-      token = token.split(" ")[1];
-    }
-
-    const decoded = jwt.verify(token, config.jwt.refreshSecret);
-    const user = await signup.findById(decoded.userId).select("-password");
-
-    if (!user) {
-      return next({
-        status: 401,
-        statusText: "Unauthorized",
-        message: "You must be logged in to access this page.",
-        errorDetails: "User missing or invalid user.",
-        loginButton: true,
-      });
-    }
-
-    req.user = user; // store user info for next middlewares
-    req.userRole = decoded.role;
-
-    next();
-  } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      return res
-        .status(401)
-        .json({ message: "Token expired, please log in again." });
-    }
-    if (error.name === "JsonWebTokenError") {
-      return res.status(401).json({ message: "Invalid token." });
-    }
-    return next({
-      status: 500,
-      statusText: "Internal Server Error",
-      message: "An unexpected error occurred on the server.",
-      errorDetails: "Check server logs for more details.",
-    });
-  }
 }
