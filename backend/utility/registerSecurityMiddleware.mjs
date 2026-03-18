b4de516fa0faba628dcb0db290c2adb709cbab98
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:55:37 +0530

    Removed the middleware due to deployment issue

--- backend/utility/registerSecurityMiddleware.mjs
+++ backend/utility/registerSecurityMiddleware.mjs
@@ -5,22 +5,22 @@
 // Security Modules
 
 // import xss from "xss-clean";
-import helmet from "helmet";
-import mongoSanitize from "express-mongo-sanitize";
-import rateLimit from "express-rate-limit";
+// import helmet from "helmet";
+// import mongoSanitize from "express-mongo-sanitize";
+// import rateLimit from "express-rate-limit";
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 1000,
-    message: "Too many requests from this IP, please try again later.",
-});
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 1000,
+//     message: "Too many requests from this IP, please try again later.",
+// });
 
-export default function securityMiddleWares(app) {
-    // Security middlewares
-    app.use([
-        // xss(),
-        helmet(),
-        mongoSanitize(),
-        limiter,
-    ]);
-}
+// export default function securityMiddleWares(app) {
+//     // Security middlewares
+//     app.use([
+//         // xss(),
+//         helmet(),
+//         mongoSanitize(),
+//         limiter,
+//     ]);
+// }
