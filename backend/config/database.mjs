a6ccdab84ff2cd871bd6a6a25d411a70f5bf9e17
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:30:13 +0530

    Debugging

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -7,10 +7,14 @@ export const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(config.mongo.uri); 
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(config.mongo.uri);
+    if (!mongoose.connect(config.mongo.uri)) {
+      console.log("Waiting....")
+    }else{
+      console.log(`MongoDB Connected ✓ --> ${config.mongo.uri}`.green);
+    }
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error("MongoDB connection failed:".red, error.message);
     process.exit(1);
   }
 };
@@ -18,7 +22,7 @@ export const connectDB = async () => {
 export async function disconnectDB() {
   try {
     await mongoose.disconnect();
-    console.log("✅ MongoDB Disconnected");
+    console.log("MongoDB Disconnected".yellow);
   } catch (err) {
     console.error("Error disconnecting MongoDB:", err.message);
   }
