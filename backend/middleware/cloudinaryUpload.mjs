e57c114b0ba8addbdf48523c7556304c58c00035
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:39:29 +0530

    Debugging

--- backend/middleware/cloudinaryUpload.mjs
+++ backend/middleware/cloudinaryUpload.mjs
@@ -1,12 +1,13 @@
 import multer from "multer";
-import cloudinary from "../config/cloudinary.mjs";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.mjs";
 import { config } from "../config/config.mjs";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: config.cloudinaryParams, // uses folder + formats from config.mjs
+  params: config.cloudinaryParams,
 });
 
 const cloudinaryUpload = multer({ storage });
+
 export default cloudinaryUpload;
