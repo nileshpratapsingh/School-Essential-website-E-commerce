03f906c6fc480966a3596a3447daf83b8926629f
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-03-19 00:22:00 +0530

    new middlewares for security

--- backend/utility/registerSecurityMiddleware.mjs
+++ backend/utility/registerSecurityMiddleware.mjs
@@ -4,23 +4,23 @@
 
 // Security Modules
 
-// import xss from "xss-clean";
-// import helmet from "helmet";
-// import mongoSanitize from "express-mongo-sanitize";
-// import rateLimit from "express-rate-limit";
-//
-// const limiter = rateLimit({
-//     windowMs: 15 * 60 * 1000,
-//     max: 1000,
-//     message: "Too many requests from this IP, please try again later.",
-// });
-//
-// export default securityMiddleWares(app){
-//     // Security middlewares
-//     app.use([
-//         xss(),
-//         helmet(),
-//         mongoSanitize(),
-//         limiter,
-//     ]);
-// }
+import xss from "xss-clean";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 1000,
+    message: "Too many requests from this IP, please try again later.",
+});
+
+export default function securityMiddleWares(app) {
+    // Security middlewares
+    app.use([
+        xss(),
+        helmet(),
+        mongoSanitize(),
+        limiter,
+    ]);
+}
