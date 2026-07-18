70addf916fa98bfb9257f4c443454179e89ef589
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-18 23:25:36 +0530

    razorpay configuration

--- backend/config/razorpay.mjs
+++ backend/config/razorpay.mjs
@@ -1,9 +1,12 @@
-import { config } from "dotenv";
+import { config } from "./config.mjs";
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-  key_id: config.razorpay.key_id,
-  key_secret: config.razorpay.key_secret,
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
-
+// RAZORPAY_KEY_ID=rzp_live_STY1hTOHNKesZF
+// RAZORPAY_KEY_SECRET=xrPqVVDcLZ6lNeeSqL0RGcyt
+console.log("razor key:",config.razorpay.key_id)
+console.log("razor secret:",config.razorpay.key_secret)
 export default razorpay;
