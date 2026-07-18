3adaf520ae42518851ee49023b163af213832dfb
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-18 23:23:02 +0530

    pgsql connection to nhost connection

--- backend/config/pgsql.mjs
+++ backend/config/pgsql.mjs
@@ -1,48 +1,39 @@
 import pkg from "pg";
 import { config } from "./config.mjs";
-const { Pool } = pkg;
-// Create a pool of connections
-export const pool = new Pool({
-    connectionString: config.postgres.url,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-// export const pool = new Pool(
-//     config.postgres.url
-//         ? {
-//             connectionString: config.postgres.url,
-//             ssl: { rejectUnauthorized: false },
-//         }
-//         : {
-//             user: config.postgres.user,
-//             host: config.postgres.host,
-//             database: config.postgres.database,
-//             password: config.postgres.password,
-//             port: config.postgres.port || 5432,
-//         },
-// );
 
-//     user: config.postgres.user,
-//     host: config.postgres.host,
-//     database: config.postgres.database,
-//     password: config.postgres.password,
-//     port: config.postgres.port || 5432,
-// });
+const { Pool } = pkg;
+
+export const pool = new Pool(
+    config.postgres.url
+        ? {
+            connectionString: config.postgres.url,
+            ssl: false
+            // {
+            //     rejectUnauthorized: false
+            // },
+        }
+        : {
+            user: config.postgres.user,
+            host: config.postgres.host,
+            database: config.postgres.database,
+            password: config.postgres.password,
+            port: config.postgres.port || 5432,
+        },
+);
 
 // Test the connection immediately
 export function connectPgSQL() {
-    pool.connect((err, client, release) => {
+    pool.connect((err,_, release) => {
         if (err) {
             console.error("Error connecting to PostgreSQL:", err.stack);
             console.log("Env variables:");
             console.log("\nURl:",config.postgres.url);
             console.log("\nUser:",config.postgres.user);
             console.log("Host:",config.postgres.host);
-            console.log("Database:",config.postgres.host);
+            console.log("Database:",config.postgres.database);
             console.log("Password:",config.postgres.password);
         } else {
-            console.log("PostgreSQL connected successfully ✓".green);
+            console.log("PostgreSQL connected successfully ✓ -->".green,config.postgres.url);
             release();
         }
     });
