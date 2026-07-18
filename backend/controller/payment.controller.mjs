c19ec26fca7cd8a3d4711f8b3fdbec077dbcf1fb
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-18 23:29:25 +0530

    payment controller implemented using razorpay.

--- backend/controller/payment.controller.mjs
+++ backend/controller/payment.controller.mjs
@@ -1,111 +1,197 @@
-import Razorpay from "razorpay";
+import razorpay from "../config/razorpay.mjs";
 import Product from "../models/product.model.mjs";
 import Order from "../models/order.model.mjs";
+import { v4 as uuidV4 } from "uuid";
+import crypto from "crypto";
 
 export class PaymentController {
+  async createPayment(req, res) {
+    try {
+      const { amount, currency } = req.body;
+      const id = uuidV4();
+      console.log("amount recived:", amount);
 
-    async createOrder(req, res, next) {
-        try {
-            const {
-                userId,
-                productIds:orderItems,
-                paymentMethod,
-                amount,
-                currency,
-            } = req.body;
+      const razorpayOrder = await razorpay.orders.create({
+        amount: Number(amount) * 100,
+        currency: currency || "INR",
+        receipt: `receipt_${id.slice(0, 20)}`,
+      });
 
-            if (!userId || !orderItems || !paymentMethod) {
-                return res.status(400).next({
-                    success: false,
-                    message: "User, orderItems and paymentMethod are required",
-                });
-            }
-
-            const product = await Product.findById(orderItems.product).lean();
-            if (!product) {
-                return res.status(404).json({
-                    success: false,
-                    message: "Product not found",
-                });
-            }
-
-            if (paymentMethod === "COD") {
-                const order = await Order.create({
-                    userId,
-                    orderItems: {
-                        product:  product._id,
-                        name:     product.name,
-                        quantity: orderItems.quantity,
-                        price:    product.price,
-                        image:    product.image,
-                    },
-                    paymentMethod,
-                    paymentResult: {
-                        status:       false,
-                        update_time:  new Date().toISOString(),
-                        email_address: "",
-                    },
-                });
-
-                return res.status(201).json({ success: true, order });
-            }
-
-            if (!amount) {
-                return res.status(400).json({
-                    success: false,
-                    message: "Amount is required for online payment",
-                });
-            }
-
-            const razorpayOrder = await Razorpay.orders.create({
-                amount:   amount * 100, // paise
-                currency: currency || "INR",
-                receipt:  `receipt_${Date.now()}`,
-            });
-
-            const order = await Order.create({
-                user,
-                orderItems: {
-                    product:  product._id,
-                    name:     product.name,
-                    quantity: orderItems.quantity,
-                    price:    product.price,
-                    image:    product.image,
-                },
-                paymentMethod,
-                paymentResult: {
-                    id:           razorpayOrder.id,
-                    status:       false,
-                    update_time:  new Date().toISOString(),
-                    email_address: "",
-                },
-                isPaid:      false,
-                orderStatus: "Pending",
-            });
-
-            res.status(201).json({
-                success:       true,
-                order,
-                razorpayOrder,
-            });
-
-        } catch (error) {
-            next(error);
-        }
+      return res.status(201).json({
+        success: true,
+        key: process.env.RAZORPAY_KEY_ID,
+        orderId: razorpayOrder.id,
+        amount: razorpayOrder.amount,
+        currency: razorpayOrder.currency,
+      });
+    } catch (error) {
+      console.error("Error Creating payment!!");
+      console.log("Check createPayment !!");
+      console.log(error);
     }
-    async findOrder(req, res) {
-        const orderId = req.body.id;
-        const found = await Order.findById(orderId);
-        res.render("pages/order",{
-            title :"Orders",
-            orders : found,
-        })
-    }
-    displayAllOrders(req,res){
+  }
 
+  async verifyPayment(req, res) {
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+      console.log("Verify body:", req.body);
+      const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+      const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+        .update(body)
+        .digest("hex");
+
+      if (expectedSignature !== razorpay_signature) {
+        return res.status(400).json({
+          success: false,
+          message: "Payment verification failed",
+        });
+      }
+
+      res.status(200).json({
+        message: "Payment Verified",
+        success: true,
+      });
+      console.log("Signature Verified!!", expectedSignature);
+    } catch (error) {
+      console.error("Error Creating payment!!");
+      console.log("Check verification !!");
+      console.log(error);
     }
-    async cancelOrder(req, res) {}
-    async displayCurrentOrder(req, res) {}
-    async displayCompletedOrder(req, res) {}
-    async verifyOrder(req,res){}
+  }
+
+  async createOrder(req, res) {
+    try {
+      const {
+        userId,
+        productIds,
+        paymentMethod,
+        amount,
+        email_address,
+        deliveryDate,
+        notes,
+        tax,
+        shipping,
+        razorpay_order_id,
+        razorpay_payment_id,
+      } = req.body;
+
+      if (!userId || !productIds || !paymentMethod) {
+        return res.status(400).next({
+          success: false,
+          message: "userId, productIds and paymentMethod are required",
+        });
+      }
+
+      const product = await Product.findById(productIds).lean();
+      // console.log("Product",product);
+      if (!product) {
+        return res.status(404).next({
+          success: false,
+          message: "Product not found",
+        });
+      }
+
+      // Cash on Delivery
+      if (paymentMethod === "COD") {
+        const order = await Order.create({
+          userId,
+          orderItems: [
+            {
+              product: product._id,
+              name: product.title,
+              quantity: 1,
+              price: product.price,
+              image: product.productImage,
+              deliverdAt: deliveryDate,
+              notes,
+              tax,
+              shipping,
+            },
+          ],
+          paymentMethod,
+          paymentResult: {
+            status: true,
+            update_time: new Date().toISOString(),
+            email_address: email_address,
+          },
+          isPaid: false,
+          orderStatus: "Pending",
+        });
+
+        return res.status(201).json({
+          success: true,
+          order,
+        });
+      }
+
+      // Online Payment
+      if (!amount) {
+        return res.status(400).json({
+          success: false,
+          message: "Amount is required",
+        });
+      }
+      console.log("Request body create order", {
+        product: product._id,
+        name: product.title,
+        quantity: 1,
+        price: product.price,
+        image: product.productImage,
+        deliverdAt: deliveryDate,
+        notes,
+        tax,
+        shipping,
+        razorpay_order_id,
+        razorpay_payment_id,
+      });
+
+      const order = await Order.create({
+        user: userId,
+        orderItems: [
+          {
+            product: product._id,
+            name: product.title,
+            quantity: 1,
+            price: product.price,
+            image: product.productImage,
+            deliverdAt: deliveryDate,
+            notes,
+            tax,
+            shipping,
+            razorpay_order_id,
+            razorpay_payment_id,
+          },
+        ],
+        paymentMethod,
+        paymentResult: {
+          id: razorpay_payment_id,
+          status: true,
+          update_time: new Date().toISOString(),
+          email_address: email_address,
+        },
+        paidAt: new Date().toISOString(),
+        isPaid: true,
+        orderStatus: "Pending",
+      });
+
+      return res.status(201).json({
+        success: true,
+        order,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+  async findOrder(req, res) {}
+  async displayAllOrders(req, res) {}
+  async cancelOrder(req, res) {}
+  async displayCurrentOrder(req, res) {}
+  async displayCompletedOrder(req, res) {}
 }
